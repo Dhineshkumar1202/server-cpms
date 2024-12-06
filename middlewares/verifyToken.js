300bc84024480d218eddc4f09ff20e6a8e59cebc
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-06 17:45:20 +0530

    files modified

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,25 +1,20 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'your_jwt_secret_key';
-
 
 const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) {
-    return res.status(403).json({ message: 'Access Denied' });
+    return res.status(401).json({ message: 'No token provided. Unauthorized.' });
   }
 
-  try {
-    
-    const tokenWithoutBearer = token.split(' ')[1];
-    const decoded = jwt.verify(tokenWithoutBearer, JWT_SECRET);
-
-   
-    req.user = decoded;
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token.' });
+    }
+    req.user = user; 
     next();
-  } catch (err) {
-    return res.status(400).json({ message: 'Invalid Token' });
-  }
+  });
 };
 
 module.exports = verifyToken;
