b8d8ee2f00272de1d6d357cf66121649e4952206
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-06 17:02:03 +0530

    files modified

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,13 +2,16 @@ const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
-
+      console.log(token)
     if (!token) {
         return res.status(401).json({ message: "Not authorized, no token" });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+       if(!decoded){
+        return res.status(401).json({ message: "Token is not valid , please do provide valid token" });
+       }
         req.user = decoded; 
         next();
     } catch (error) {
