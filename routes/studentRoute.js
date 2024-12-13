5c6e686a440b614fa82a9a22c5a5b2bf0fa499bc
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-14 03:58:00 +0530

    files changed

--- routes/studentRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getStudentProfile } = require('../controllers/studentController');
-const { protect } = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.get('/profile', protect, getStudentProfile);
-
-module.exports = router;
