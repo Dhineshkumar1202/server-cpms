3c6f20d0449e1087716a2456cf22ba86cf660614
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-14 03:42:56 +0530

    files updated

--- models/studentModel.js
+++ models/studentModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  name:{type: String, required: true},
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   phone: { type: String },
