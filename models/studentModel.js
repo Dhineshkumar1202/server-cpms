119ff7208a78891ebe815522593128443eccd833
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-14 03:46:00 +0530

    files changed

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name:{type: String, required: true},
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: String },
-  resume: { type: String },  // File link or file path
-  applicationStatus: { type: String, enum: ['submitted', 'under review', 'shortlisted', 'rejected', 'accepted'], default: 'submitted' },
-  interviewSchedule: { type: Date },
+  name: {
+    type: String,
+    required: true, // Ensuring that the 'name' field is required
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // other fields...
 });
 
 const Student = mongoose.model('Student', studentSchema);
-
 module.exports = Student;
