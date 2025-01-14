4fe220bc69ce80a4a1cc7719e96d53b0343e1926
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-01-14 12:45:22 +0530

    files updated

--- models/studentModel.js
+++ models/studentModel.js
@@ -12,6 +12,32 @@ const studentSchema = new mongoose.Schema({
         required: true,
         enum: ['1st Year', '2nd Year', '3rd Year', '4th Year'],
     },
+    contact: { 
+        type: String, 
+        required: false, 
+        default: '' 
+    }, // Student's contact number
+    academicRecords: {
+        grades: { type: String, default: '' }, 
+        achievements: { type: [String], default: [] }, 
+    },
+    resume: { 
+        type: String, 
+        default: '' 
+    }, // File path or URL for the resume
+    applications: [
+        {
+            status: { 
+                type: String, 
+                enum: ['Submitted', 'Reviewed', 'Shortlisted'], 
+                default: 'Submitted' 
+            },
+            interviewSchedule: { 
+                type: Date, 
+                default: null 
+            },
+        },
+    ],
 });
 
 module.exports = mongoose.model('Student', studentSchema);
