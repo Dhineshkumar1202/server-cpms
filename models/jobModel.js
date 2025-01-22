6e4587b4d1811bca9383437a15edcce75d7f0719
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-01-22 20:23:53 +0530

    files updated

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,26 +1,12 @@
-const mongoose = require("mongoose");
-
 const jobSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
+    {
+      title: { type: String, required: true },
+      description: { type: String, required: true },
+      subject: { type: String, required: true },
+      location: { type: String, required: true },
+      applicationDeadline: { type: Date, required: true },
+      postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     },
-    description: {
-      type: String,
-      required: true,
-    },
-    subject: {
-      type: String,
-      required: true,
-    },
-    postedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Job", jobSchema);
+    { timestamps: true }
+  );
+  
