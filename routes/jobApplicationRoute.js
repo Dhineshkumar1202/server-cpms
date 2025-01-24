59347134956e7432765014ba18489d36a62ead35
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-01-24 22:54:52 +0530

    files updated

--- routes/jobApplicationRoute.js
+++ routes/jobApplicationRoute.js
@@ -9,7 +9,7 @@ const JobApplication = require("../models/jobApplicationModel");
 // Configure Multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Folder to save files
+    cb(null, "uploads/"); 
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
